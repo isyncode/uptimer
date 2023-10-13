df1e73fcf0e8d09a3328243795d0995206ef6801
isyncode <133251800+isyncode@users.noreply.github.com>
2023-10-14 01:20:49 +0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,29 @@
+import requests
+import time
+from fastapi import FastAPI
+import asyncio
+import os
+import aiohttp
+from dotenv import load_env
+load_env()
+
+app = FastAPI()
+
+start = False
+
+@app.get("/")
+async def main():
+
+    if not start:
+        start = True
+        asyncio.create_task(run())
+
+    return "Program Start"
+
+
+async def run():
+    while True:
+        async with aiohttp.ClientSession() as session:
+            request = await session.get(os.environ.get("URL"))
+            print(request.status)
+            time.sleep(30)

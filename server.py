451ab25851d8ef4bed380a990b70038363aeb30f
isyncode <133251800+isyncode@users.noreply.github.com>
2023-10-14 01:41:40 +0800

    Update server.py

--- server.py
+++ server.py
@@ -14,20 +14,11 @@ start = False
 
 @app.get("/")
 async def main():
-    global start
-    if not start:
-        start = True
-        asyncio.create_task(run())
+    async with aiohttp.ClientSession() as session:
+        request = await session.get(os.environ.get("URL"))
+        print(request.status)
 
     return "Program Start"
-
-
-async def run():
-    while True:
-        async with aiohttp.ClientSession() as session:
-            request = await session.get(os.environ.get("URL"))
-            print(request.status)
-            time.sleep(30)
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0")
